479ac3e45cf5db95d63a5d576e528534584dedd3
Your Name <artemnesterov1991@gmail.com>
2020-10-29 22:28:05 +0200

    init

---
+++ shop/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+"""
+Описание моделей 
+"""
+
+
+class User(AbstractUser):
+    cash = models.FloatField(max_length=10)  # сумма денег у пользователя
+
+
+class Product(models.Model):
+    item = models.PositiveIntegerField(null=False, unique=True)  # раздел
+    price = models.DecimalField(max_digits=8, decimal_places=2)  # цена
+    in_stock = models.BooleanField()  # на складе остаток
+    description = models.CharField(max_length=500)  # описание
+    image = models.CharField(max_length=500)  # картинка
+
+    def __str__(self):
+        return str(self.item)
+
+
+class Image(models.Model):
+    image = models.CharField(max_length=500)
+    product = models.ForeignKey(Product, related_name='images')
---
+++ shop/views.py
@@ -0,0 +1,93 @@
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView, FormView
+
+from shop.forms1 import UserCreateForm
+from shop.models import Product
+
+"""
+Ссылка, на которой будет отображаться продукт
+"""
+
+
+class MainView(TemplateView):
+    template_name = 'main.html'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Если пользователь аутифинцирован, то показать список продуктов(товара)
+        """
+        if request.user.is_authenticated:
+            products = Product.objects.all()
+            ctx = {'products': products}
+            return render(request, self.template_name, ctx)
+        else:
+            return render(request, self.template_name, {})
+
+
+"""
+Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации,
+указана ссылка на страницу входа для зарегистрированных пользователей.
+"""
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreateForm
+    """
+    Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации.
+    Указана ссылка на страницу входа для зарегистрированных пользователей.
+    """
+    template_name = 'register.html'
+    success_url = '/login/'
+
+
+def form_valid(self, form):
+    """
+    Создаём пользователя, если данные в форму были введены корректно.
+    """
+    form.save()
+    return super(RegisterFormView, self).form_valid(form)
+
+
+def form_invalid(self, form):
+    """
+    Если данные введены не верно, возврат на регистрацию
+    """
+    return super(RegisterFormView, self).form_invalid(form)
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        """
+        получение объекта пользователя
+        """
+        self.user = form.get_user()
+
+        """
+        аутентификация пользователя
+        """
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(LoginFormView, self).form_invalid(form)
+
+
+class LogoutView(View):
+    """
+    Выполняем выход для пользователя
+    """
+
+    def get(self, request):
+        logout(request)
+        """
+        Перенаправляем пользователя на главную страницу.
+        """
+
+        return HttpResponseRedirect('/')

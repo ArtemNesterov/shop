f98722fda3bf3ff45adb0dcc49937733bc2b3b62
Your Name <artemnesterov1991@gmail.com>
2020-11-01 23:25:27 +0200

    fix migrations, add models

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, ItemType, MyUser
 from .models import Image
 
 admin.site.register(Product)
 admin.site.register(Image)
+admin.site.register(MyUser)
+admin.site.register(ItemType)
--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 """
@@ -6,21 +6,33 @@ from django.db import models
 """
 
 
-class MyUser(AbstractUser):
-    cash = models.FloatField(max_length=10)  # сумма денег у пользователя
-
-
-class Product(models.Model):
-    item = models.PositiveIntegerField(null=False, unique=True)  # раздел
-    price = models.DecimalField(max_digits=8, decimal_places=2)  # цена
-    in_stock = models.BooleanField()  # на складе остаток
-    description = models.CharField(max_length=500)  # описание
-    image = models.CharField(max_length=500)  # картинка
-
-    def __str__(self):
-        return str(self.item)
+class MyUser(User):
+    cash = models.DecimalField(max_digits=8, decimal_places=2)  # сумма денег у пользователя
 
 
 class Image(models.Model):
-    image = models.CharField(max_length=500)
-    product = models.ForeignKey(Product, related_name='images', on_delete='')
+    image = models.ImageField()
+
+    def __str__(self):
+        return str(self.product)
+
+
+class ItemType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Product(models.Model):
+    count = models.PositiveIntegerField()
+    item = models.ForeignKey(ItemType, on_delete=models.CASCADE)  # раздел
+    price = models.DecimalField(max_digits=8, decimal_places=2)  # цена
+    in_stock = models.BooleanField(default=True)  # на складе остаток
+    description = models.CharField(max_length=500)  # описание
+    image = models.ForeignKey(Image, related_name='product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        from datetime import datetime as datetime
+        now = str(datetime.now())
+        return '{}-{}'.format(self.item, now)

1837823f284bb0b9e777b26d5a0a8fa730ade93e
Your Name <artemnesterov1991@gmail.com>
2020-11-16 20:59:26 +0200

    add crispy

--- my_modul/settings.py
+++ my_modul/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-    'rest_framework'
+    'rest_framework',
+    'crispy_forms',
 
 ]
 
